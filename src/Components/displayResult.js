be77f48cc241ef389d2056ec478135a42e5eba87
David_Izosimov <David.Izosimov@gmail.com>
2019-10-14 13:28:36 +0200

    Adds BMICalculator module (logic), and adds BMICalculator to displayResult component.

--- src/Components/displayResult.js
+++ src/Components/displayResult.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { bmiCalculation } from '../Modules/BMICalculator'
 
 class DisplayResult extends Component {
   calculate() {
     let weight= this.props.weight;
     let height= this.props.height;
+
+    return bmiCalculation(weight, height);
   }
 
   render() {
